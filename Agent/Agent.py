Fixed lightgbm numerical issues

Agent/Agent.py
@@ -1,14 +1,17 @@
 import os
 
-import keras.backend as K
-from keras import losses
 import numba as nb
 import numpy as np
-from keras.layers import Input, Dense, Flatten, PReLU, BatchNormalization, Conv2D, UpSampling2D, MaxPool2D
-from keras.models import Model
+import pickle
 
+import keras.backend as K
+from keras.losses import mean_squared_error
+from keras.layers import Input, Dense, Flatten, PReLU, BatchNormalization, Conv2D, UpSampling2D, MaxPool2D, GlobalMaxPooling2D
+from keras.models import Model
+from random import randint
+
+from PriorityExperienceReplay import PriorityExperienceReplay
 from Environnement.Environnement import Environnement
-from LSTM_Model import LSTM_Model
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
@@ -28,11 +31,8 @@ def policy_loss(actual_value, predicted_value, old_prediction):
         r = K.mean(prob / (old_prob + 1e-10))
 
         entropy = K.sum(y_pred * K.log(y_pred + 1e-10), axis=-1)
-
-        # So every element still looks like himself, iunno if that is all that needed
-        mse_loss = losses.mean_squared_error(y_true=y_true, y_pred=y_pred)
-
-        return mse_loss -log_prob * K.mean(K.minimum(r * advantage, K.clip(r, min_value=0.8, max_value=1.2) * advantage)) + 0.01 * entropy
+        mse = K.mean(mean_squared_error(y_true, y_pred))
+        return mse - log_prob * K.mean(K.minimum(r * advantage, K.clip(r, min_value=0.8, max_value=1.2) * advantage)) + 0.01 * entropy
     return loss
 
 
@@ -40,15 +40,18 @@ class Agent:
     def __init__(self, training_epochs=10, from_save=False, amount_per_class=10, amount_of_examples=1000):
         self.training_epochs = training_epochs
         self.environnement = Environnement(amount_per_class=amount_per_class, amount_of_examples=amount_of_examples)
+        self.priority_replay = PriorityExperienceReplay(500)
         self.batch_size = amount_per_class * 10
-
-        self.training_data = [[], [], [], []]
 
         # Bunch of placeholders values
         self.dummy_value = np.zeros((self.batch_size, 1))
         self.dummy_predictions = np.zeros((self.batch_size, 28, 28, 1))
 
         self.actor_critic = self._build_actor_critic()
+        self.critic_losses, self.policy_losses, self.classification_losses = [], [], []
+
+        self.replay_policy_losses, self.replay_critic_losses, self.replay_classification_losses = [], [], []
+
 
         if from_save is True:
             self.actor_critic.load_weights('actor_critic')
@@ -56,6 +59,7 @@ class Agent:
     def _build_actor_critic(self):
 
         state_input = Input(shape=(28, 28, 1))
+        # class_input = Input(shape=(1,))
 
         # Used for loss function
         actual_value = Input(shape=(1,))
@@ -72,29 +76,31 @@ class Agent:
         main_network = BatchNormalization()(main_network)
         main_network = MaxPool2D()(main_network)
 
-        main_network = Conv2D(64, (3,3), padding='same')(main_network)
+        main_network = Conv2D(128, (3,3), padding='same')(main_network)
         main_network = PReLU()(main_network)
         main_network = BatchNormalization()(main_network)
         main_network = UpSampling2D()(main_network)
 
-        main_network = Conv2D(128, (3,3), padding='same')(main_network)
+        main_network = Conv2D(256, (3,3), padding='same')(main_network)
         main_network = PReLU()(main_network)
         main_network = BatchNormalization()(main_network)
+        main_network = UpSampling2D()(main_network)
 
-        actor_next_word = UpSampling2D()(main_network)
-        actor_next_word = Conv2D(1, (3,3), padding='same', activation='sigmoid')(actor_next_word)
+        actor = Conv2D(1, (3,3), padding='same', activation='sigmoid')(main_network)
 
-        critic = Flatten()(main_network)
+        flat = GlobalMaxPooling2D()(main_network)
 
-        critic_value = Dense(1)(critic)
+        critic = Dense(1)(flat)
+        input_class = Dense(10, activation='softmax')(flat)
 
-        actor_critic = Model(inputs=[state_input, actual_value, predicted_value, old_predictions], outputs=[actor_next_word, critic_value])
+        actor_critic = Model(inputs=[state_input, actual_value, predicted_value, old_predictions], outputs=[actor, critic, input_class])
         actor_critic.compile(optimizer='adam',
                       loss=[policy_loss(actual_value=actual_value,
                                         predicted_value=predicted_value,
                                         old_prediction=old_predictions
                                         ),
-                            'mse'
+                            'mse',
+                            'sparse_categorical_crossentropy'
                             ])
 
         actor_critic.summary()
@@ -102,7 +108,7 @@ class Agent:
 
     def train(self, epoch):
 
-        value_list, policy_losses, critic_losses = [], [], []
+        value_list, policy_losses, critic_losses, classification_losses = [], [], [], []
         e = 0
         while e <= epoch:
             done = False
@@ -112,30 +118,67 @@ class Agent:
 
 
                 batch_x, batch_f1, batch_y, done = self.environnement.query_state()
-                old_predictions, predicted_values = self.actor_critic.predict([batch_x, self.dummy_value, self.dummy_value, self.dummy_predictions])
+                old_predictions, predicted_values, _ = self.actor_critic.predict([batch_x, self.dummy_value, self.dummy_value, self.dummy_predictions])
                 actions = get_actions(old_predictions)
                 values = self.get_values(actions, batch_f1, batch_y)
                 value_list.append(np.mean(values))
 
 
-                tmp_loss = np.zeros(shape=(self.training_epochs, 2))
+                tmp_loss = np.zeros(shape=(self.training_epochs, 3))
                 for i in range(self.training_epochs):
                     tmp_loss[i] = (self.actor_critic.train_on_batch([batch_x, values, predicted_values, old_predictions],
-                                                                    [actions, values])[1:])
+                                                                    [actions, values, batch_y])[1:])
                 policy_losses.append(np.mean(tmp_loss[:,0]))
                 critic_losses.append(np.mean(tmp_loss[:,1]))
+                classification_losses.append(np.mean(tmp_loss[:,2]))
+                # self.priority_replay.add_elem(critic_losses[-1], [batch_x, values, predicted_values, old_predictions, actions, values, batch_y])
 
 
-                if batch_num % 500 == 0:
-                    print()
+                if batch_num % 50 == 0:
+                    # self.play_priority_replay(100)
+                    self.save_losses(critic_losses, policy_losses, classification_losses)
+                    self.print_most_recent_losses(batch_num, e, value_list)
+                    value_list, policy_losses, critic_losses, classification_losses = [], [], [], []
+
                     self.actor_critic.save_weights('actor_critic')
-                    print('Batch number :', batch_num, '\tEpoch :', e, '\tAverage values :', np.mean(value_list))
-                    print('Policy losses :', '%.5f' % np.mean(policy_losses),
-                          '\tCritic losses :', '%.5f' % np.mean(critic_losses))
-                    value_list, policy_losses, critic_losses = [], [], []
 
                 batch_num += 1
             e += 1
+
+    def play_priority_replay(self, n=100):
+        self.replay_policy_losses = []
+        self.replay_critic_losses = []
+        self.replay_classification_losses = []
+        priority_batches = self.priority_replay.get_n_largest(n)
+
+        for batches in priority_batches:
+            for i in range(self.training_epochs):
+                tmp_loss = np.zeros(shape=(self.training_epochs, 3))
+                tmp_loss[i] = (self.actor_critic.train_on_batch(
+                    [batches[1][0], batches[1][1], batches[1][2], batches[1][3]],
+                    [batches[1][4], batches[1][5], batches[1][6]])[1:])
+            self.replay_policy_losses.append(np.mean(tmp_loss[:, 0]))
+            self.replay_critic_losses.append(np.mean(tmp_loss[:, 1]))
+            self.replay_classification_losses.append(np.mean(tmp_loss[:, 2]))
+
+    def save_losses(self, critic_losses, policy_losses, classification_losses):
+        self.critic_losses.append(np.mean(critic_losses))
+        self.policy_losses.append(np.mean(policy_losses))
+        self.classification_losses.append(np.mean(classification_losses))
+        pickle.dump(self.critic_losses, open('critic_loss.pkl', 'wb'))
+        pickle.dump(self.policy_losses, open('policy_loss.pkl', 'wb'))
+        pickle.dump(self.classification_losses, open('classification_loss.pkl', 'wb'))
+
+    def print_most_recent_losses(self, batch_num, e, value_list):
+        print()
+        print('Batch number :', batch_num, '\tEpoch :', e, '\tAverage values :', np.mean(value_list))
+        print('Policy losses :', '%.5f' % self.policy_losses[-1],
+              '\tCritic losses :', '%.5f' % self.critic_losses[-1],
+              '\tClassification losses :', '%.5f' % self.classification_losses[-1],
+              '\tReplay Policy losses :', '%.5f' % np.mean(self.replay_policy_losses),
+              '\tReplay Critic losses :', '%.5f' % np.mean(self.replay_critic_losses),
+              '\tReplay Classification losses :', '%.5f' % np.mean(self.replay_classification_losses))
+
 
     @nb.jit
     def get_values(self, actions, batch_f1, batch_y):
@@ -156,12 +199,10 @@ def get_actions(old_predictions):
         for j in range(actions.shape[1]):
             for k in range(actions.shape[2]):
                 actions[i,j,k,0] = np.random.choice([0,1], 1, p=[old_predictions[i,j,k,0], 1-old_predictions[i,j,k,0]])[0]
+
     return actions
 
 
-
-
-
 if __name__ == '__main__':
-    agent = Agent()
+    agent = Agent(amount_per_class=15, amount_of_examples=25000)
     agent.train(epoch=5000)

Environnement/Environnement.py
@@ -2,7 +2,6 @@ import numpy as np
 import numba as nb
 from lightgbm import LGBMClassifier
 import os
-from time import time
 from keras.datasets import mnist
 from sklearn.metrics import f1_score
 import warnings
@@ -17,7 +16,8 @@ class Environnement:
         # Keeping em positive
         self.dataset_x /= 255.0
 
-        self.model = LGBMClassifier(objective='multiclass', num_class=10)
+        self.model = LGBMClassifier(objective='multiclass', num_class=10, n_jobs=1, min_child_samples=1,
+                                    min_child_weight=0, min_data_in_bin=1, verbosity=-1, verbose=-1)
 
     def get_values(self, actions, targets):
         actions = np.reshape(actions, (actions.shape[0], 28*28)) * 255.0
@@ -46,42 +46,41 @@ class Environnement:
 
 def create_mnist_dataset(amount_per_class=50, how_many_training_examples=5000000):
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
     if os.path.isfile('dataset_x_' + str(amount_per_class) + '_' + str(how_many_training_examples) + '.npy'):
         dataset_x = np.load('dataset_x_' + str(amount_per_class) + '_' + str(how_many_training_examples) + '.npy')
         dataset_y = np.load('dataset_y_' + str(amount_per_class) + '_' + str(how_many_training_examples) + '.npy')
         dataset_f1 = np.load('dataset_f1_' + str(amount_per_class) + '_' + str(how_many_training_examples) + '.npy')
 
     else:
-        x_train_lgbm = np.reshape(x_train, (x_train.shape[0], 28 * 28))
-        x_test_lgbm = np.reshape(x_test, (x_test.shape[0], 28 * 28))
+        x_train_gbm = np.reshape(x_train, (x_train.shape[0], 28 * 28))
+        x_test_gbm = np.reshape(x_test, (x_test.shape[0], 28 * 28))
         dataset_x = np.zeros((how_many_training_examples, amount_per_class * 10, 28, 28))
         dataset_y = np.zeros((how_many_training_examples, amount_per_class * 10))
         dataset_f1 = np.zeros((how_many_training_examples, 10))
 
         class_indexes = [np.where(y_train == i) for i in range(10)]
+        choices = [np.array([np.random.choice(class_index[0], amount_per_class) for class_index in class_indexes]).flatten() for _ in range(how_many_training_examples)]
 
-
-        start = time()
-        model = LGBMClassifier(objective='multiclass', num_class=10)
+        # Training with n_jobs >1 causes segfaults, lightgbm is faster then itself
+        # This is definitely hardware dependant, has no issue on my laptop
+        model = LGBMClassifier(objective='multiclass', num_class=10, n_jobs=1, min_child_samples=1,
+                               min_child_weight=1e-10, min_data_in_bin=1, verbosity=-1, verbose=-1)
+        # model = XGBClassifier(objective='multi:softmax', n_estimators=50)
         for i in range(how_many_training_examples):
-            choices = [np.random.choice(class_index[0], amount_per_class) for class_index in class_indexes]
-            choices = np.array(choices).flatten()
-
-            model.fit(x_train_lgbm[choices], y_train[choices])
-            pred = model.predict(x_test_lgbm)
-
-            dataset_x[i] = x_train[choices]
-            dataset_y[i] = y_train[choices]
+            model.fit(x_train_gbm[choices[i]], y_train[choices[i]])
+            pred = model.predict(x_test_gbm)
+            dataset_x[i] = x_train[choices[i]]
+            dataset_y[i] = y_train[choices[i]]
             dataset_f1[i] = f1_score(y_test, pred, average=None)
+            print(i)
 
-        print(time() - start)
-
-    np.save('dataset_x_' + str(amount_per_class) + '_' + str(how_many_training_examples), dataset_x)
-    np.save('dataset_y_' + str(amount_per_class) + '_' + str(how_many_training_examples), dataset_y)
-    np.save('dataset_f1_' + str(amount_per_class) + '_' + str(how_many_training_examples), dataset_f1)
+        np.save('dataset_x_' + str(amount_per_class) + '_' + str(how_many_training_examples), dataset_x)
+        np.save('dataset_y_' + str(amount_per_class) + '_' + str(how_many_training_examples), dataset_y)
+        np.save('dataset_f1_' + str(amount_per_class) + '_' + str(how_many_training_examples), dataset_f1)
 
     return dataset_x, dataset_y, dataset_f1, np.reshape(x_test, (x_test.shape[0], 28*28)), y_test
 
 if __name__ == '__main__':
-    env = Environnement(10, 2500)
+    env = Environnement(15, 50000)
     print(env.query_state(2))